2d223546e8ae5f06b2c9dc3946176f4be5b2e05a
shimizu <noon3m.op.sh@gmail.com>
2024-03-31 00:39:58 +0900

    [add]ログイン画面のルーティング

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
 import { Terms } from "./pages/Terms";
 import Registration from "./pages/Registration";
+import Login from "./pages/Login";
+import { useEffect } from "react";
 
 export const Routers = () => {
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      window.location.pathname = "/login";
+    }
+  }, []);
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/terms" element={<Terms />}></Route>
           <Route path="/registration" element={<Registration />}></Route>
+          <Route path="/login" element={<Login />}></Route>
         </Routes>
       </BrowserRouter>
     </>
