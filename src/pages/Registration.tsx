174fe3dd852ec7af95718403c615d70f9297f3e6
mattuu <tempacountyt@gmail.com>
2024-04-09 19:36:22 +0900

    iroiro

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -22,8 +22,8 @@ const Registration: React.FC = () => {
 
   useEffect(() => {
     // ページにアクセスしたときに名前がなければ/termsにリダイレクト
-    fetch("/app/userinfo",{
-      "method" : "GET"
+    fetch("/app/userinfo", {
+      method: "GET",
     }).then((res) => {
       res.json().then((data) => {
         var user = data["user"];
@@ -36,21 +36,28 @@ const Registration: React.FC = () => {
 
         //それ以外ならログインさせる
         navigate("/login");
-      })
+      });
 
       if (res.status != 200) {
         navigate("/login");
       }
-    })
+    });
     // dropプラグインを適用
+    $("h1").drop();
     $("h1").drop();
   }, [sendName.className, navigate]);
 
   return (
     <>
-      <div className="flex h-full w-full text-purple-300 justify-center display:none">
-        <h1 className="m-0 p-0 text-xl md:text-5xl lg:text-7xl">ようこそIoT部へ!!</h1>
-        <a href=""></a>
+      <div className="flex h-full w-full flex-col items-center justify-center text-purple-300">
+        <h1 className="m-0 p-0 text-xl md:text-5xl lg:text-7xl">
+          <p>ようこそIoT部へ!!</p>
+        </h1>
+        <a href="https://discord.gg/dNeJ5UNrvW">
+          <h1 className="mt-16 p-0 text-center text-xl text-blue-300 md:text-5xl lg:text-7xl">
+            参加
+          </h1>
+        </a>
       </div>
     </>
   );
